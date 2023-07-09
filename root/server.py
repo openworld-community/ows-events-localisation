a76889deeaed66f4d368cbfe11533540d2f2a0f0
Natalia Lyzhina <natlyz96@yandex.ru>
2023-07-09 11:26:18 +0300

    OWS-L-3 Add md5 hash function to text comparison and move db connection to start application point

--- root/server.py
+++ root/server.py
@@ -1,6 +1,7 @@
 from path import Path
 
 from flask import Flask, request, abort
+from flask_wtf.csrf import CSRFProtect
 from dotenv import load_dotenv
 from flask_cors import CORS
 import urllib.parse
@@ -15,7 +16,7 @@ def search_text(text, table, language, cursor):
             SELECT {table}.translated_text
             FROM {table}
             WHERE source_text='{text}'
-            AND target_language='{language}'
+            AND target_language=md5('{language}')
             AND translated_text IS NOT NULL;
             """
     cursor.execute(sql)
@@ -46,7 +47,8 @@ def cache_text(text, table, language, result, cursor):
     cursor.execute(sql)
 
 def is_authorized(token_from_request, token_to_validate):
-    if token_from_request==token_to_validate:
+    # two validations in case both tokens are None for some reason
+    if token_from_request and token_from_request==token_to_validate:
         return True
     return False
 
@@ -58,30 +60,13 @@ def create_app():
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
 
-    @app.route("/")
-    def hello():
-        # TODO: Move creation of connection to a separate function and call it not in every request
+    def get_db_connection():
         USER = os.getenv("USER")
         PASSWORD = os.getenv("PASSWORD")
         HOST = os.getenv("HOST")
         PORT = os.getenv("PORT")
         DB = os.getenv("DB")
-        TABLE = os.getenv("TABLE")
-        AUTH = os.getenv("AUTH")
-
-        authorization_header = request.headers.get('Authorization')
-
-        if not is_authorized(
-                token_to_validate = AUTH,
-                token_from_request = authorization_header
-            ):
-            abort(403)
-
-        args = request.args
-
-        text = args.get('text')
-        target_language = args.get('tl')
-
+        
         try:
             conn = pg.connect(
                 host=HOST,
@@ -93,16 +78,38 @@ def create_app():
             )
             conn.autocommit = True
         except Exception as e:
-            print(e)
+            return None
+        return conn
+
+    @app.route("/")
+    def hello():
+        TABLE = os.getenv("TABLE")
+        AUTH = os.getenv("AUTH")
+        authorization_header = request.headers.get('Authorization')
+
+        if not is_authorized(
+                token_to_validate = AUTH,
+                token_from_request = authorization_header
+            ):
+            abort(403)
+
+        conn = get_db_connection()
+        if not conn:
             return "No database connection"
+
+        args = request.args
+
+        text = args.get('text')
+        target_language = args.get('tl')
         
         if not text:
             return "No text"
 
         if not target_language:
             return "No target language"
-
-        text = text.replace("'", "--quote--")
+        
+        # TODO: Add parsing (new lines and so on)
+        # text = text.replace("'", "--quote--")
         
         cursor = conn.cursor()
         
